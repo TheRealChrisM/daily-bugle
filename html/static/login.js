c75029e550665320069aef0ad15f894c7b8a95ad
Chris Marotta <ctm5vjg@virginia.edu>
2023-11-30 03:35:16 -0500

    preparing static pages.

---
+++ html/static/login.js
@@ -0,0 +1,13 @@
+function initPage(){
+    //prepare a onclick function for the login button
+
+    
+}
+
+function submitLogin(){
+    //collect currently entered username
+
+    //collect currently entered password
+
+    //make post request to /api/dailybugle/auth/login
+}
